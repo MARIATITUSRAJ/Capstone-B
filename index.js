d35b88ab8e292208fe15f17526c58496a40b24b9
MARIATITUSRAJ <titusraj30@gmail.com>
2025-01-08 20:37:27 +0530

    message

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import cookieparser from 'cookie-parser'
-import DbCon from './utlis/db.js'
-import AuthRoutes from './routes/Auth.js'
-import AdminRoutes from './routes/AdminRoutes.js'
-dotenv.config()
-const PORT=process.env.PORT || 2000
-const app=express()
+const express= require("express")
+const dotenv= require("dotenv").config()
+const cors= require("cors")
+const {mongoose} =require('mongoose')
+const cookieParser= require('cookie-parser')
+const { urlencoded } = require("body-parser")
+const jwt = require('jsonwebtoken');
 
-// mongo db 
-DbCon()
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(()=>console.log("Database Connnected."))
+    .catch((err)=>console.log("Some Error While connecting",err))
+
+
+const app = express();
+//middleware
 app.use(express.json())
-app.use(cookieparser())
+app.use(cookieParser())
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('Public'))
+
 app.use(cors({
+    methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true,
-    origin: 'http://localhost:5173'  
+    origin: "http://localhost:5173", 
 }));
 
-app.use('/api/auth',AuthRoutes)
-app.use('/api/admin',AdminRoutes)
 
-app.get('/',(req,res)=>{
-    res.send('test')
-})
+app.use('/',require('./routes/authRoutes'))
+app.use('/',require('./routes/empRoutes'))
 
-app.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}`)
+const verifyuser = (req,res,next)=>{
+    const token=req.cookies.token;
+    if(token){
+        jwt.verify(token,process.env.JWT_SECRET, (error,decoded)=>{
+            if(error) return res.json({Status:false,Error:"Wrong Token"})
+            req.id = decoded.id;
+            req.role = decoded.role;
+            next()
+        })
+    } else{
+        res.json({Status:false,Error:"Not Authenticated"})
+    }
+}
+app.get('/verify',verifyuser,(req,res)=>{
+    return res.json({Status:true, role:req.role, id:req.id})
 })
+app.get('/',(req, res) => {
+    res.send('The backend is running Successfully');
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
--- models/user.js
+++ models/user.js
@@ -1,28 +1,114 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
-const userSechmea= new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique: true
-    },
-    role:{
-        type:String,
-        enum:['admin',"user"],
-        default:"user"
-    },
-    password:{
-        type:String,
-        required:true
-    }
-},{timestamps:true})
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  salary: Number,
+  address: String,
+  category: String,
+  position: String,
+  image: String,
+  dateofbirth: Date,
+  gender: String,
+  martialStatus: String,
+  bankAccount: Number,
+  IFSC: String,
+  taxId: Number,
+  postGraduation: String,
+  graduation: String,
+  schooling: String,
+  hobbies: String,
+  doj: Date,
+});
+
+const newEmployeeSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  salary: Number,
+  address: String,
+  category: String,
+  position: String,
+  image: String,
+  dateofbirth: Date,
+  gender: String,
+  martialStatus: String,
+  bankAccount: Number,
+  IFSC: String,
+  taxId: Number,
+  postGraduation: String,
+  Graduation: String,
+  Schooling: String,
+  hobbies: String,
+  doj: Date,
+});
+
+const LeaveSchema = new mongoose.Schema({
+  empid: String,
+  email: String,
+  name: String,
+  fromdate: Date,
+  todate: Date,
+  reason: String,
+  status: {
+    type: String,
+    default: "Pending",
+  },
+});
+
+const HistorySchema = new mongoose.Schema({
+  companyName: String,
+  jobTitle: String,
+  startDate: String,
+  endDate: String,
+  jobDescription: String,
+  userId: String,
+});
+
+const eventSchema = new mongoose.Schema({
+  title: String,
+  date: Date,
+  additional: String,
+});
+
+const positionSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+const trainingModuleSchema = new mongoose.Schema({
+  name: String,
+  file: String,
+  department: String,
+});
+
+const employeeModuleSchema = new mongoose.Schema({
+  employeeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'newEmployee',
+    required: true
+  },
+  moduleId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'TrainingModule',
+    required: true
+  },
+  completedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 
-const UserModel= mongoose.model('users',userSechmea)
 
+const userModel = mongoose.model("User", userSchema)
+const newEmployeeModel = mongoose.model("newEmployee", newEmployeeSchema)
+const LeaveModel = mongoose.model("newLeave", LeaveSchema)
+const HisotryModel = mongoose.model("newHistory", HistorySchema)
+const eventModel = mongoose.model("newEvent", eventSchema)
+const Position = mongoose.model('Position', positionSchema);
+const TrainingModule = mongoose.model('TrainingModule', trainingModuleSchema);
+const empModule = mongoose.model('empModule', employeeModuleSchema);
 
-export default UserModel
+module.exports = { userModel, newEmployeeModel, LeaveModel, HisotryModel, eventModel, Position, TrainingModule, empModule };
